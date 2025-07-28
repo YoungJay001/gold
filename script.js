69fb9af4e8b320db30d9478dd8f11cd610f09138
YoungJay <13284568+youngjay123@user.noreply.gitee.com>
2025-07-28 17:45:53 +0800

    度量衡

--- script.js
+++ script.js
@@ -240,9 +240,7 @@ class TreasureHunter {
     }
 
     formatValue(value) {
-        if (value >= 1000000) {
-            return (value / 1000000).toFixed(1) + 'M';
-        } else if (value >= 1000) {
+        if (value >= 1000) {
             return (value / 1000).toFixed(1) + 'K';
         }
         return value.toString();
